bc6ec2edd68ca4aba07218109623f3cd8751eb92
edwin <edwin.lingson@gmail.com>
2023-09-13 11:06:26 -0400

    first commit

---
+++ src/main/java/com/etr/MedicalProject/MedicalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.etr.MedicalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/etr/MedicalProject/MedicalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etr.MedicalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
